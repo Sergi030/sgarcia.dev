5521da52824f49e2f3d28d870f70a5901e9de733
Your Name <you@example.com>
2020-08-09 01:55:01 +0200

    Adding code for sgarcia.dev

---
+++ content/posts/example.md
@@ -0,0 +1,12 @@
+---
+title: "Example"
+date: 2020-08-07T02:07:31+02:00
+draft: true
+---
+
+# Markdown is fully supported
+
+## So are a variety of other flavors
+
+If you prefer certain flavors, you should check it
+out on the official documentation.
---
+++ deploy.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+checkCommand () {
+    if [ $? -ne 0 ]
+    then
+        echo "$1"
+        exit 1
+    fi
+
+}
+
+fancyMessage () {
+    printf "\033[0;32m$1\033[0m\n"
+}
+
+if [ "$1" == "nobuild" ]
+then
+    NOBUILD="nobuild"
+fi
+
+
+if [ -z $NOBUILD ]
+then
+    fancyMessage "Building site"
+    # Build the project.
+    hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+fi
+
+# Go To Public folder
+cd public
+
+# Check if there is any change
+git diff-index --quiet HEAD
+if [ $? -eq 0 ]
+    then
+
+    # Add changes to git.
+    fancyMessage "Adding Changes"
+    git add .
+    checkCommand "Cannot add any change"
+
+    # Commit changes.
+    fancyMessage "Commit the new build"
+    msg="rebuilding site $(date)"
+    if [ -n "$*" ]; then
+        msg="$*"
+    fi
+    git commit -m "$msg"
+    checkCommand "Cannot commit the changes"
+
+    # Push source and build repos.
+    fancyMessage "Pushing changes"
+    git push origin master
+    checkCommand "Cannot push changes"
+fi
