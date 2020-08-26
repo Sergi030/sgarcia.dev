a131bf37f9320d05ba4ec9cae48246cdb35e78f7
Tu Nombre <you@example.com>
2020-08-26 08:17:35 +0200

    Nav bar on mobile now has social links

--- themes/owntheme/static/js/script.js
+++ themes/owntheme/static/js/script.js
@@ -1,6 +1,22 @@
-toggleButton = document.getElementsByClassName('toggle-button')[0]
-navbarLinks = document.getElementsByClassName('navbar-links')[0]
+toggleButton = document.getElementsByClassName('toggle-button')[0];
+navbarLinks = document.getElementsByClassName('navbar-links')[0];
+navbarBurger = document.getElementById("navbar-burger");
+navbarCross = document.getElementById("navbar-cross");
+navbarSocial = document.getElementById("navbar-social");
+
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
+    navbarLinks.classList.toggle('active');
+
+    if (navbarCross.style.display == "none" ){
+        // menu is closed, we want to open it
+        navbarBurger.style.display="none";
+        navbarCross.style.display="block";
+        navbarSocial.style.display="block"
+    } else {
+        // menu is open, we want to close it
+        navbarCross.style.display="none";
+        navbarBurger.style.display="block";
+        navbarSocial.style.display="none"
+    }
 })
